64ccf17ba9545c1edfb5dc1818b1a6000d5bc8aa
Yuan Gao <meseta@gmail.com>
2013-09-15 02:34:43 +0100

    accounted for weird windows CMD bug

--- Loader/lpcloader.bat
+++ Loader/lpcloader.bat
@@ -1,5 +1,9 @@
 @echo off
 
+rem for some reason this needs to go up here or it doesn't work, no idea why
+set lpcpath=
+for /f %%D in ('wmic volume get DriveLetter^, Label ^| find "CRP DISABLD"') do set lpcpath=%%D
+
 if "%1" == "" (
 	set filepath=firmware.bin
 ) else (
@@ -10,17 +14,17 @@ if not exist %filepath% (
 	echo Firmware file %filepath% not found
 ) else (
 	echo ...Detecting LPC device
-	set lpcpath=
-	for /f %%D in ('wmic volume get DriveLetter^, Label ^| find "CRP DISABLD"') do set lpcpath=%%D
 	
 	if "%lpcpath%" == "" (
 		echo LPC device not detected
 	) else (
 		echo 	Device found at %lpcpath%
 		echo ...Removing old firmware
-		del %lpcpath%\firmware.bin
-		if not exist %lpcpath%\firmware.bin (
+		if exist %lpcpath%\firmware.bin (
+			del %lpcpath%\firmware.bin
 			echo 	Removed old firmware
+		) else (
+			echo 	No old firmware found, that's unusual!
 		)
 		
 		echo ...Copying new firmware
@@ -29,3 +33,5 @@ if not exist %filepath% (
 		echo ...Finished
 	)
 )
+
+pause
